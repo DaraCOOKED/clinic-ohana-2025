0065c9f1280b20cbc623ac97712385a77f36d957
DaraCOOKED <dara.somnag020@gmail.com>
2025-12-26 08:03:16 +0700

    header

--- themes/my-theme/header.php
+++ themes/my-theme/header.php
@@ -1,12 +1,8 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head><?php wp_head(); ?></head>
+<link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/style.css">
 <body <?php body_class(); ?>>
-    <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/style.css">
-<header>
-
-
-
 <header class="clinic-header">
   <div class="header-inner">
     <div class="logo">
@@ -16,18 +12,20 @@
     <nav class="main-nav">
       <a href="#">Home</a>
       <a href="#">First Visit</a>
-      <a href="#">About Us</a>
+      <a href="#">About</a>
       <a href="#">Services</a>
-      <a href="#">Contact</a>
       <a href="#">Access</a>
+      <a href="#">Contact</a>
     </nav>
 
     <div class="header-cta">
-      <a href="#" class="phone">03-3987-3090</a>
-      <a href="#" class="book">Web予約</a>
+      <span class="phone">03-3987-3090</span>
+      <a href="#" class="book">Web Booking</a>
     </div>
   </div>
 </header>
+
+
 
 
 
