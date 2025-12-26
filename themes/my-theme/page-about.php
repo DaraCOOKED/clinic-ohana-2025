da453053fba2c1b1071cf33c210d76341ffda486
bora4-netizen <chorkbora4@gmail.com>
2025-12-26 07:09:02 +0700

    hi

---
+++ themes/my-theme/page-about.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>About-Page</title>
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/.style/about-page.css">
+
+</head>
+<body>
+	<?php get_header(); ?>
+	
+	
+
+
+	
+	<?php get_footer(); ?>
+
+</body>
+</html>
