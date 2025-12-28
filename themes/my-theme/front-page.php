6765bedf1ace89890e8fbd04a702e0129331ae0f
bora4-netizen <chorkbora4@gmail.com>
2025-12-28 13:52:52 +0700

    Update front-page.php to include Font Awesome stylesheet for icon support

--- themes/my-theme/front-page.php
+++ themes/my-theme/front-page.php
@@ -2,7 +2,7 @@
 
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/.style/main.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css" />
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
 
 
 
