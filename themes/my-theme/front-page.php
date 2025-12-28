623b1fe5e1679cf8848bf02f1d5fffba5bf1f1ed
DaraCOOKED <dara.somnag020@gmail.com>
2025-12-28 09:02:16 +0700

    Remove conflicting stylesheet link in front-page.php

--- themes/my-theme/front-page.php
+++ themes/my-theme/front-page.php
@@ -10,7 +10,6 @@
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/.style/main.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-
 </head>
 <body>
 
